059c6f210101ec6b5f8b275340a71d06fcc3032e
Boo-coder <Bouna112@yahoo.com>
2024-08-24 19:46:54 -0400

    done3

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+(cats);
+
+function array () {
+  cats.length = 0;
+  cats.push("Milo", "Otis", "Garfield");
+}
+function destructivelyAppendCat(name) {
+   cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+   cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newArray = cats.slice();
+    newArray.push("Broom");
+    return newArray;
+}
+
+function prependCat(name) {
+    const newArray = cats.slice();
+    newArray.unshift("Arnold");
+    return newArray;
+}
+
+function removeLastCat() {
+    const newArray = cats.slice();
+    newArray.pop();
+    return newArray;
+}
+
+function removeFirstCat() {
+    const newArray =cats.slice();
+    newArray.shift();
+    return newArray;
+}
